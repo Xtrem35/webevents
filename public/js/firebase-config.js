c3eba87db839c9b6d34d12df181c2d8c1faa98c4
XTREM35 <sieur.thierry.gogo@gmail.com>
2023-09-24 17:51:36 +0000

    Mon premier commit

---
+++ public/js/firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.3.1/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBo-2Q-evSLHPwDvzG8H58rF5vizdtNVpU",
+  authDomain: "events-8f2f6.firebaseapp.com",
+  projectId: "events-8f2f6",
+  storageBucket: "events-8f2f6.appspot.com",
+  messagingSenderId: "691731348542",
+  appId: "1:691731348542:web:53b493ad6aafe8948da56f"
+};
+
+const app = initializeApp(firebaseConfig);
